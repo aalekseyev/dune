7339274b82505b947f61fb4c4fc67cf7296afedc
Jeremie Dimino <jeremie@dimino.org>
2019-04-25 20:31:20 +0200

    Move get_installed_binaries to super_context.ml
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/install_rules0.mli
+++
@@ -1,7 +0,0 @@
-open! Stdune
-
-val get_installed_binaries
-  :  Stanza.t list Dir_with_dune.t list
-  -> context:Context.t
-  -> expander:(dir:Path.t -> Expander.no_env Expander.t_gen)
-  -> Path.Set.t
--- src/super_context.ml
+++ src/super_context.ml
@@ -310,6 +310,31 @@ let resolve_program t ~dir ?hint ~loc bin =
   let bin_artifacts = Env.bin_artifacts_host t ~dir in
   Artifacts.Bin.binary ?hint ~loc bin_artifacts bin
 
+let get_installed_binaries stanzas ~(context : Context.t) ~expander =
+  let install_dir = Config.local_install_bin_dir ~context:context.name in
+  Dir_with_dune.deep_fold stanzas ~init:Path.Set.empty ~f:(fun d stanza acc ->
+    match (stanza : Stanza.t) with
+    | Dune_file.Install { section = Bin; files; _ } ->
+      let expand_str = Expander.expand_str (expander ~dir:d.ctx_dir) in
+      List.fold_left files ~init:acc ~f:(fun acc fb ->
+        match
+          match File_binding.Unexpanded.expand_dst fb ~f:expand_str with
+          | None ->
+            Some
+              (File_binding.Unexpanded.expand_src fb ~dir:d.ctx_dir
+                 ~f:expand_str
+               |> Path.basename)
+          | Some p ->
+            if Path.Local.is_root (Path.Local.parent_exn p) then
+              Some (Path.Local.basename p)
+            else
+              None
+        with
+        | None -> acc
+        | Some basename ->
+          Path.Set.add acc (Path.relative install_dir basename))
+    | _ -> acc)
+
 let create
       ~(context:Context.t)
       ?host
@@ -395,7 +420,7 @@ let create
       bin =
         Artifacts.Bin.create ~context
           ~local_bins:(
-            Install_rules0.get_installed_binaries
+            get_installed_binaries
               ~context
               ~expander:(fun ~dir ->
                 Expander.set_dir expander0 ~dir)
